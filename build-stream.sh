ae205cb1cdb41193abc0480312452fecde3e1179
chenzhihui244 <chenzhihui244@msn.com>
2019-06-29 15:51:18 +0800

    add transparent hugepage

--- build-stream.sh
+++ build-stream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-gcc -O3 -fopenmp -DSTREAM_ARRAY_SIZE=80000000 -DNTIMES=10 stream.c -static -o stream
+build_stream() {
+	gcc -O3 -fopenmp -DSTREAM_ARRAY_SIZE=80000000 -DNTIMES=10 stream.c -o stream
+
+	#yum install -y glibc-static.aarch64
+	#gcc -O3 -fopenmp -DSTREAM_ARRAY_SIZE=80000000 -DNTIMES=10 stream.c -static -o stream
+}
+
+build_stream
--- test-stream.sh
+++ test-stream.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-#export OMP_NUM_THREADS=1
-#./stream
-
-#export OMP_NUM_THREADS=128
-#./stream
-
-stream_mp_test () {
+stream_mp_test_1c () {
 	export OMP_NUM_THREADS=1
 	export GOMP_CPU_AFFINITY=0
 	./stream
+}
 
-
+stream_mp_test_128c () {
 	export OMP_NUM_THREADS=128
 	export GOMP_CPU_AFFINITY=0-127
 	./stream
 }
 
+# always enable thp
+echo always > /sys/kernel/mm/transparent_hugepage/enabled
+echo always > /sys/kernel/mm/transparent_hugepage/defrag
+
 for i in 1 2 3 4 5; do
-	stream_mp_test
+	stream_mp_test_1c
+	sleep 5
+	stream_mp_test_128c
 	sleep 5
 done 
